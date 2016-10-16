5be2608bf1858b5eb9075847294aee76e33d9dcc
root <brandonhinds13@gmail.com>
2016-10-16 15:41:37 -0400

    Added Multiple Tile Slide

--- fifteen.js
+++ fifteen.js
@@ -1,6 +1,7 @@
 
 //Extra Done: Animation (Grade This)
 //Extra Done: Game Time
+//Extra Done: Slide Rows and Columns
 
 $(document).ready(function(){
 	$("#puzzlearea div").addClass("puzzlepiece");
@@ -51,7 +52,7 @@ $(document).ready(function(){
 
 		//Glow piece if hovered
 		$(pieces[i]).on("mouseover", function(){
-			if(validate(this)){ $(this).addClass("movablepiece"); }
+			if(validate(this) || multiMV(this)){ $(this).addClass("movablepiece"); }
 		});
 
 		//If mouse leaves don't remove movable class
@@ -65,8 +66,80 @@ $(document).ready(function(){
 				switchTile(this, true);
 				moves++;
 			}
+			else if(multiMV(this)){ moveMultiple(this); }
 		});
 	}
+
+	//Test if row/column can be moved
+	var multiMV = function(piece){
+		if(parseInt($(piece).css("top")) == blanky || parseInt($(piece).css("left")) == blankx){
+			return true;
+		}
+		else{ return false; }
+	};
+
+	//Move multiple tiles in the same row or column
+	var moveMultiple = function(piece){
+
+		function helpFind(x, pos, cond){
+			var all = [];
+
+			for(var i=0; i < pieces.length; i++){
+				var dis = Math.abs(parseInt($(pieces[i]).css("left")) - blankx);
+				var dis2 = Math.abs(parseInt($(pieces[i]).css("top")) - blanky);
+
+				var diff = Math.abs(parseInt($(pieces[i]).css("left")) - parseInt($(piece).css("left")));
+				var dif2 = Math.abs(parseInt($(pieces[i]).css("top")) - parseInt($(piece).css("top")));
+
+				if (pos === "row" && parseInt($(pieces[i]).css("top")) == x && dis <= cond && diff <= 200){
+					all.push(pieces[i]);
+				}
+				else if (pos === "col" && parseInt($(pieces[i]).css("left")) == x && dis2 <= cond && dif2 <= 200){
+					all.push(pieces[i]);
+				}
+			}
+
+			return all;
+		}
+
+		if(parseInt($(piece).css("top")) == blanky){
+			//move row
+			var x = parseInt($(piece).css("top"));
+
+			var dis = Math.abs(parseInt($(piece).css("left")) - blankx);
+
+			var allrow = helpFind(x, "row", dis);
+			var len = allrow.length;
+
+			for(var c=0; c < len; c++){
+			    for(var i=0; i < len; i++){
+				    if(validate(allrow[i])){
+				        switchTile(allrow[i], true); 
+				        allrow.splice(i, 1);
+				    }
+			    }
+		    }
+		}
+
+		else if(parseInt($(piece).css("left")) == blankx){
+			//move column
+			var x = parseInt($(piece).css("left"));
+
+			var dis = Math.abs(parseInt($(piece).css("top")) - blanky);
+
+			var allrow = helpFind(x, "col", dis);
+			var len = allrow.length;
+
+			for(var c=0; c < len; c++){
+			    for(var i=0; i < len; i++){
+				    if(validate(allrow[i])){
+				        switchTile(allrow[i], true); 
+				        allrow.splice(i, 1);
+				    }
+			    }
+		    }
+		}
+	};
 
 	//Test if tiles are near blank tile
 	var validate = function(piece){
@@ -79,7 +152,7 @@ $(document).ready(function(){
 		else{ return false; }
 	};
 
-	//function to switch
+	//function to switch (move: tile to move, anim: whether to animate or not)
 	var switchTile = function(move, anim){
 		var tempx = blankx;
 		var tempy = blanky;
